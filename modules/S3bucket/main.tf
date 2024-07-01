13f4cdfbaf3b81ee4fa0890cae56ed441b1e5da8
ShellHoff <142273690+ShellHoff@users.noreply.github.com>
2024-07-01 10:41:41 -0400

    first s3 bucket

--- modules/S3bucket/main.tf
+++ modules/S3bucket/main.tf
@@ -1,1 +1,31 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "eu-west-2"
+}
+terraform {
+  backend "s3" {
+    bucket = "USPS_Dev"
+    key    = "Usps_key"
+    region = "eu-west-2"
+  }
+}
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "[USPS-Dev]"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::[USPS-Dev]/*",
+      "Principal": "*"
+    }
+  ]
+}
 
