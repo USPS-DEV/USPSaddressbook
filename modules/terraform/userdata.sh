2a47e6d4cf3359e495767523bb8f1ac816daf6e5
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-08-10 23:45:39 -0400

    Update userdata.sh

--- modules/terraform/userdata.sh
+++ modules/terraform/userdata.sh
@@ -3,10 +3,13 @@
 sudo apt-get update -y
 sudo apt-get install -y curl apt-transport-https gnupg lsb-release
 sudo apt-get install -y openjdk-21-jdk 
+
 # Download and install HashiCorp GPG key
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
 # Add HashiCorp official repository
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+
 # Update apt cache and install Terraform
 sudo apt-get update
 sudo apt-get install terraform -y
